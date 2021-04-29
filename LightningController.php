64db82d95653310b12cbff65c598b88038e5af0c
supertestnet <58400631+supertestnet@users.noreply.github.com>
2021-04-29 19:26:04 -0400

    Add files via upload
    These are the basic files that comprise the wordpress plugin

---
+++ LightningController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Handlers\ResponseData;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class LightningController
+{
+    // delete LNBits wallet
+    // take wallet info as param, return nothing
+    public function deleteLNBitsWallet( $lnbits_url, $wallet_id, $lnbits_apikey )
+    {
+        ob_start();
+        $url = $lnbits_url . '/usermanager/api/v1/wallets/' . $wallet_id;
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+                'X-Api-Key: ' . $lnbits_apikey 
+        ));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+        $head = curl_exec( $ch );
+        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        curl_close( $ch );
+        $data = ob_get_clean();
+        return $data;
+    }
+
+    // generate a LNBits wallet
+    // take lnbits url, lnbits api key, user id, wallet name, and admin id as params, return wallet info. Note that before this function can work, your lnbits instance must have the user manager extension installed and one user created. Once that is done, your user_id can be found in the Users section of the User Manager extension page and your admin_id can be found in the url of the User Manager extension page after the parameter "usr=". Also note that one user can have many wallets so you can have a user called something like "Lightning Video Game"Ã‚Â and then each player can have a unique wallet belonging to that one user.
+    public function generateLNBitsWallet( $lnbits_url, $user_id, $wallet_name, $admin_id, $lnbits_apikey )
+    {
+        ob_start();
+        $payload = '{"user_id": "' . $user_id . '", "wallet_name": "' . $wallet_name . '", "admin_id": "' . $admin_id . '"}';
+        $url = $lnbits_url . '/usermanager/api/v1/wallets';
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+                'X-Api-Key: ' . $lnbits_apikey,
+                'Content-Type: application/json'
+        ));
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+        $head = curl_exec( $ch );
+        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        curl_close( $ch );
+        $data = ob_get_clean();
+        return $data;
+    }
+
+    // pay a Lightning invoice using an LNBits wallet
+    // take lnbits url, invoice, and lnbits api key as params, return success or error or failure
+    public function payInvoice( $lnbits_url, $invoice, $lnbits_apikey )
+    {
+        ob_start();
+        $payload = '{"out": true, "bolt11": "' . $invoice . '"}';
+        $url = $lnbits_url . '/api/v1/payments';
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+                'X-Api-Key: ' . $lnbits_apikey,
+                'Content-Type: application/json'
+        ));
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+        $head = curl_exec( $ch );
+        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        curl_close( $ch );
+        $data = ob_get_clean();
+	$data = json_decode( $data, true );
+        if ( $data["payment_hash"] ) {
+                return 1;
+        }
+	if ( !$data["payment_hash"] && $data["message"] ) {
+		$error["error"] = $data["message"];
+		return json_encode( $error );
+	} else {
+        $error["error"] = json_encode( $data );
+        return json_encode( $error );
+    }
+        return 0;
+    }
+
+    // request a Lightning invoice from a LNBits wallet
+    // take lnbits url, amount, memo, and lnbits api key as params, return generated invoice and payment hash
+//    public function requestInvoice( $lnbits_url, $amount, $memo, $lnbits_apikey, $webhook )
+    public function requestInvoice( $lnbits_url, $amount, $memo, $lnbits_apikey )
+    {
+        ob_start();
+//        $payload = '{"out": false, "amount": ' . $amount . ', "memo": "' . $memo . '", "webhook": "' . $webhook . '"}';
+//        $payload = '{"num_satoshis":' . $amount . ', "memo":' . $memo . '}';
+	$payload = '{"num_satoshis":' . $amount . ',"memo":"' . $memo . '"}';
+//        $url = $lnbits_url . '/api/v1/payments';
+        $url = 'https://api.lnpay.co/v1/wallet/' . $lnbits_apikey . '/invoice';
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+//                'X-Api-Key: ' . $lnbits_apikey,
+                'Content-Type: application/json'
+        ));
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+//The following line was not originally part of LightningController.php
+        curl_setopt($ch, CURLOPT_USERPWD, $lnbits_apikey . ":");
+        $head = curl_exec( $ch );
+        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        curl_close( $ch );
+        $data = ob_get_clean();
+        $data = json_decode( $data, true );
+        $result["invoice"] = $data["payment_request"];
+//        $result["pmthash"] = $data["payment_hash"];
+        $result["pmthash"] = $data["id"];
+        $json = json_encode( $result );
+        return $json;
+    }
+
+    // check a Lightning invoice from a LNBits wallet
+    // take lnbits url, payment hash, and lnbits api key as params, return 1 if paid or 0 otherwise
+    public function checkInvoice( $lnbits_url, $pmthash, $lnbits_apikey )
+    {
+        ob_start();
+//        $url = $lnbits_url . '/api/v1/payments';
+        $url = 'https://api.lnpay.co/v1/lntx/' . $pmthash . '?fields=settled';
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+/*        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+                'X-Api-Key: ' . $lnbits_apikey,
+                'Content-Type: application/json'
+        ));
+*/
+//The following line was not originally part of LightningController.php
+        curl_setopt($ch, CURLOPT_USERPWD, "$lnbits_apikey:");
+        $head = curl_exec( $ch );
+        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        curl_close( $ch );
+//        $paychecker = ob_get_clean();
+        $data = ob_get_clean();
+//The following 3 lines were not originally part of LightningController.php
+        $data = json_decode( $data, true );
+        $paychecker = $data["settled"];
+	return $paychecker;
+        if ( $paychecker == 1 ) {$paychecker = "true";}
+        if ( strpos( $paychecker, "true" ) !== false ) {
+            $paychecker = 1;
+        } else {
+            $paychecker = 0;
+        }
+        return $paychecker;
+    }
+}
+
+?>
---
+++ articlevotes.php
@@ -0,0 +1,257 @@
+<?php
+
+/**
+ * Plugin Name: Article Votes
+ * Description: Manage a database of articles such that people can vote for them by sending sats
+ * Version: 1.0.0
+ * Author: Super Testnet
+ */
+
+function articlesDB() {
+        global $wpdb;
+        $articles_db_version = '1.0';
+        $table_name = $wpdb->prefix . "articlesDB";
+        $charset_collate = $wpdb->get_charset_collate();
+        $sql = "CREATE TABLE $table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                title text(50),
+                img text(50),
+                sats bigint(16),
+                trending tinyint(1),
+                description text(350),
+                link text(50),
+                status tinyint(1),
+                PRIMARY KEY  (id)
+        ) $charset_collate;";
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+        add_option( 'articles_db_version', $articles_db_version );
+}
+
+register_activation_hook( __FILE__, 'articlesDB' );
+
+function addArticle() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'articlesDB';
+	$title = $_GET[ "title" ];
+        $img = $_GET[ "img" ];
+        $sats = $_GET[ "sats" ];
+        $trending = $_GET[ "trending" ];
+        $description = $_GET[ "description" ];
+	$link = $_GET[ "link" ];
+	$status = $_GET[ "status" ];
+        $wpdb->insert(
+                $table_name,
+                array(
+                        'title' => $title,
+                        'img' => $img,
+                        'sats' => $sats,
+                        'trending' => $trending,
+			'description' => $description,
+			'link' => $link,
+                        'status' => $status,
+                )
+        );
+}
+
+add_action('wp_ajax_addarticle', 'addArticle');
+
+function showArticles( $atts = array() ) {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'articlesDB';
+	$selection = "SELECT * FROM $table_name";
+	if ( $atts[ "status" ] == "true" ) {
+		$selection = "SELECT * FROM $table_name WHERE status = 1";
+	}
+        $articles = $wpdb->get_results( $selection );
+	return json_encode( $articles, true );
+}
+
+add_shortcode( 'showarticles', 'showArticles' );
+
+function deleteArticle() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'articlesDB';
+        $id = $_GET[ "article_id" ];
+	$wpdb->update( $table_name, array( 'status' => 0 ), array( 'id' => $id ) );
+}
+
+add_action('wp_ajax_deletearticle', 'deleteArticle');
+
+function editArticle() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'articlesDB';
+        $id = $_GET[ "article_id" ];
+        $title = $_GET[ "title" ];
+        $img = $_GET[ "img" ];
+        $sats = $_GET[ "sats" ];
+        $trending = $_GET[ "trending" ];
+        $description = $_GET[ "description" ];
+        $link = $_GET[ "link" ];
+	$status = 1;
+        $wpdb->update( $table_name, array(
+		'title' => $title,
+                'img' => $img,
+                'sats' => $sats,
+                'trending' => $trending,
+                'description' => $description,
+                'link' => $link,
+                'status' => $status,
+	), array( 'id' => $id ) );
+}
+
+add_action('wp_ajax_editarticle', 'editArticle');
+
+function checkInvoice() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'invoicesDB';
+        include_once( 'LightningController.php' );
+        $lnbits_url = get_option( 'lnbits_url' );
+        $pmthash = $_GET[ "pmthash" ];
+        $title = $_GET[ "title" ];
+        $lnbits_apikey = get_option( 'lnbits_api_key' );
+        $LightningController = new App\Http\Controllers\LightningController();
+        $status = $LightningController->checkInvoice( $lnbits_url, $pmthash, $lnbits_apikey );
+	if ( $status == 1 ) {
+		$wpdb->update( $table_name, array( 'status' => 0 ), array( 'pmthash' => $pmthash ) );
+	}
+        $selection = "SELECT amount, article FROM $table_name WHERE pmthash = '" . $pmthash . "'";
+        $invoicedata = $wpdb->get_results( $selection );
+        $newsats = $invoicedata[0]->amount;
+        $article = $invoicedata[0]->article;
+        $table_name = $wpdb->prefix . 'articlesDB';
+        $selection = "SELECT sats FROM $table_name WHERE title = '" . $article . "'";
+        $oldsats = $wpdb->get_results( $selection );
+        $oldsats = $oldsats[0]->sats;
+        $totalsats = $oldsats + $newsats;
+        if ( $status == 1 ) {
+                $wpdb->update( $table_name, array( 'sats' => $totalsats ), array( 'title' => $article ) );
+        }
+        if ( $status == 1 ) {
+	        $selection = "SELECT id, trending FROM $table_name";
+        	$trending = $wpdb->get_results( $selection );
+        	foreach( $trending as $item ) {
+        	        $wpdb->query( "UPDATE wp_articlesDB SET trending = 0" );
+	        }
+                $wpdb->update( $table_name, array( 'trending' => 1 ), array( 'title' => $title ) );
+        }
+        echo $status;
+	die();
+}
+
+add_action( 'wp_ajax_checkinvoice', 'checkInvoice' );
+add_action( 'wp_ajax_nopriv_checkinvoice', 'checkInvoice' );
+
+function getInvoice() {
+    $amt = 10;
+    $memo = "Test Invoice";
+    if ( isset( $_GET[ "amount" ] ) ) {
+        $amt = $_GET[ "amount" ];
+    }
+    if ( isset( $_GET[ "memo" ] ) ) {
+        $memo = $_GET[ "memo" ];
+    }
+    include_once('LightningController.php');
+    $lnbits_url = get_option( 'lnbits_url' );
+    $amount = $amt;
+    $lnbits_apikey = get_option( 'lnbits_api_key' );
+    $webhook = "https://www.bitcoinaudible.com/";
+    $LightningController = new App\Http\Controllers\LightningController();
+    $invoice = $LightningController->requestInvoice( $lnbits_url, $amount, $memo, $lnbits_apikey, $webhook );
+    $data = json_decode( $invoice, true );
+    $pmtrqst = $data[ "invoice" ];
+    $pmthash = $data[ "pmthash" ];
+    addInvoice( $pmtrqst, $amount, $memo, $pmthash );
+    echo $invoice;
+    die();
+}
+
+add_action( 'wp_ajax_getinvoice', 'getInvoice' );
+add_action( 'wp_ajax_nopriv_getinvoice', 'getInvoice' );
+
+function invoicesDB() {
+        global $wpdb;
+        $invoices_db_version = '1.1';
+        $table_name = $wpdb->prefix . "invoicesDB";
+        $charset_collate = $wpdb->get_charset_collate();
+        $sql = "CREATE TABLE $table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                invoice text(250),
+		amount bigint(16),
+                article text(250),
+                pmthash text(64),
+                status tinyint(1),
+                PRIMARY KEY  (id)
+        ) $charset_collate;";
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+        add_option( 'invoices_db_version', $invoices_db_version );
+}
+
+register_activation_hook( __FILE__, 'invoicesDB' );
+
+function addInvoice( $invoice, $amount, $article, $pmthash ) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'invoicesDB';
+        $wpdb->insert(
+                $table_name,
+                array(
+                        'invoice' => $invoice,
+			'amount' => $amount,
+			'article' => $article,
+			'pmthash' => $pmthash,
+                        'status' => 1,
+                )
+        );
+}
+
+function articlevotes_register_settings() {
+	add_option( 'lnbits_url', '' );
+	add_option( 'lnbits_api_key', '' );
+	register_setting( 'articlevotes_options_group', 'lnbits_url', 'articlevotes_callback' );
+	register_setting( 'articlevotes_options_group', 'lnbits_api_key', 'articlevotes_callback' );
+}
+add_action( 'admin_init', 'articlevotes_register_settings' );
+
+function articlevotes_register_options_page() {
+	add_options_page('Article Votes', 'Article Votes', 'manage_options', 'articlevotes', 'articlevotes_options_page');
+}
+add_action('admin_menu', 'articlevotes_register_options_page');
+
+function articlevotes_options_page()
+{
+?>
+        <h2 style="text-decoration: underline;">Article Votes</h2>
+        <form method="post" action="options.php">
+                <?php settings_fields( 'articlevotes_options_group' ); ?>
+		<h3>
+			LNBits Settings
+		</h3>
+		<table>
+			<tr valign="middle">
+				<th scope="row">
+					<label for="lnbits url">
+						LNBits url
+					</label>
+				</th>
+				<td>
+					<input type="text" id="lnbits_url" name="lnbits_url" value="<?php echo get_option('lnbits_url'); ?>" placeholder="http://localhost:5000" />
+				</td>
+			</tr>
+		</table>
+		<table>
+			<tr valign="middle">
+				<th scope="row">
+					<label for="lnbits api key">
+						LNBits api key
+					</label>
+				</th>
+				<td>
+					<input type="text" id="lnbits_api_key" name="lnbits_api_key" value="<?php echo get_option('lnbits_api_key'); ?>" placeholder="57sd8Qi6FYr1eZJMyd43tJmmmj35Lb4h" />
+				</td>
+			</tr>
+		</table>
+                <?php  submit_button(); ?>
+        </form>
+<?php
+} ?>
